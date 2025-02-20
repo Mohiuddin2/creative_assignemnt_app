b5e5897821584df9b5191a1ac9e31c1ed45dcdce
Mohiuddin2 <mohibdrs@gmail.com>
2025-02-20 21:12:41 +0600

    resize the max width

--- app/published-blog/[blogID]/page.tsx
+++ app/published-blog/[blogID]/page.tsx
@@ -19,7 +19,7 @@ export default async function BlogPostPage({ params }: { params: { blogID: strin
   if (!post) return notFound(); // Show 404 page if no post is found
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-8 bg-slate-100 text-black rounded w-3/4">
       <h1 className="text-3xl font-bold">{post.title}</h1>
       <p className="text-sm text-gray-500">
         By {post.author} - {new Date(post.createdAt).toLocaleDateString()}
