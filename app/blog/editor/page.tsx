3fdfe1db057a516999a93b2a5cc7ed89969f5de8
Mohiuddin2 <mohibdrs@gmail.com>
2025-02-23 09:08:57 +0600

    pop up model

--- app/blog/editor/page.tsx
+++ app/blog/editor/page.tsx
@@ -4,8 +4,8 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../../components/ui/button";
-// import { useState } from "react";
-// import { ClipLoader } from "react-spinners"; // Importing ClipLoader from react-spinners
+import { useState } from "react";
+import { ClipLoader } from "react-spinners"; // For loading spinner
 
 import {
   Form,
@@ -24,7 +24,7 @@ function extractTextFromHTML(html: string) {
 }
 
 const formSchema = z.object({
-  title: z.string().min(3, { message: "Title must be at least 3 characters" }), 
+  title: z.string().min(3, { message: "Title must be at least 3 characters" }),
   content: z.string().refine(
     (value) => {
       return extractTextFromHTML(value).trim().length >= 5;
@@ -38,60 +38,59 @@ const formSchema = z.object({
 });
 
 export default function Home() {
-  // const [loading, setLoading] = useState(false)
+  const [isModalOpen, setModalOpen] = useState(false); // State to control the modal visibility
+  const [loading, setLoading] = useState(false);
+  
   const form = useForm({
     mode: "onTouched",
-    resolver: zodResolver(formSchema), // ✅ Now validates `title` too
+    resolver: zodResolver(formSchema),
     defaultValues: {
-      title: "", // ✅ Ensure title has a default value
+      title: "",
       content: "",
       author: "",
       tags: [],
     },
   });
 
-  const onSubmit = async (data:any) => {
-    console.log("Data Submitted:", data);
-    // setLoading(true); // Show the spinner
-  
+  const onSubmit = async (data: any) => {
+    setLoading(true); // Show loading spinner
+    
     try {
-      // Perform the POST request to the /api/blog/ endpoint
       const response = await fetch("/api/blog/", {
-        method: "POST", // Set the method to POST
+        method: "POST",
         headers: {
-          "Content-Type": "application/json", // Set content type to JSON
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data), // Stringify the data to send as JSON
+        body: JSON.stringify(data),
       });
-  
+
       if (!response.ok) {
         throw new Error("Failed to submit blog data");
       }
-  
-      // If response is successful, handle the response (e.g., show a success message)
+
       const responseData = await response.json();
       console.log("API Response:", responseData);
-  
-      // Do something with the response (e.g., navigate to another page or show a success message)
-    } catch (error) {
-      // Handle errors (e.g., network issues or server-side errors)
-      console.error("Error submitting data:", error);
-    } 
-    // finally {
-    //   // setLoading(false); // Hide the spinner after request completes
 
-    // }
+      // Open the modal after successful submission
+      setModalOpen(true);
+
+      // Reset the form to its default state
+      form.reset();
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    } finally {
+      setLoading(false); // Hide loading spinner
+    }
   };
 
   return (
     <div className="max-w-3xl mx-auto py-5">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-      
           <FormField
             control={form.control}
             name="author"
-            render={({ field}) => (
+            render={({ field }) => (
               <FormItem>
                 <FormLabel>Author</FormLabel>
                 <FormControl>
@@ -106,24 +105,23 @@ export default function Home() {
             )}
           />
           <FormField
-              control={form.control}
-              name="tags"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Tags (comma-separated)</FormLabel>
-                  <FormControl>
-                    <input
-                      type="text"
-                      value={field.value.join(", ")} // Convert array to string
-                      onChange={(e) => field.onChange(e.target.value.split(",").map(tag => tag.trim()))} // Convert string to array
-                      className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-slate-100 text-black"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
+            control={form.control}
+            name="tags"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tags (comma-separated)</FormLabel>
+                <FormControl>
+                  <input
+                    type="text"
+                    value={field.value.join(", ")} // Convert array to string
+                    onChange={(e) => field.onChange(e.target.value.split(",").map(tag => tag.trim()))} // Convert string to array
+                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-slate-100 text-black"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="title"
@@ -141,8 +139,6 @@ export default function Home() {
               </FormItem>
             )}
           />
-
-
           <FormField
             control={form.control}
             name="content"
@@ -159,172 +155,31 @@ export default function Home() {
               </FormItem>
             )}
           />
-
           <Button type="submit" className="mt-4 bg-slate-50 text-black">
-            Submit
+            {loading ? <ClipLoader size={20} color="#000" /> : "Submit"}
           </Button>
         </form>
       </Form>
+
+      {/* Modal for successful submission */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-emerald-300 bg-opacity-75 flex justify-center items-center z-50 transition-opacity duration-500 ease-in-out opacity-100">
+          <div
+            className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full transform scale-100 transition-all duration-500 ease-out"
+            style={{ transform: isModalOpen ? "scale(1)" : "scale(0.95)", opacity: isModalOpen ? 1 : 0 }}
+          >
+            <h2 className="text-2xl font-bold text-center text-gray-800">Your content is Created. Thank you.</h2>
+            <div className="mt-4 flex justify-center">
+              <Button
+                onClick={() => setModalOpen(false)}
+                className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-700 transition-colors"
+              >
+                Close
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-// "use client";
-
-// import React from "react";
-// import RichTextEditor from "../../components/Blog/BlogEditor";
-// import { useForm, ControllerRenderProps } from "react-hook-form";
-// import * as z from "zod";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { Button } from "../../components/ui/button";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "../../components/ui/form";
-
-// // Helper function to extract plain text from HTML
-// function extractTextFromHTML(html: string): string {
-//   const div = document.createElement("div");
-//   div.innerHTML = html;
-//   return div.textContent || div.innerText || "";
-// }
-
-// // Define the form schema using Zod
-// const formSchema = z.object({
-//   title: z.string().min(3, { message: "Title must be at least 3 characters" }),
-//   content: z.string().refine(
-//     (value) => extractTextFromHTML(value).trim().length >= 5,
-//     { message: "The text must be at least 5 characters long after trimming" }
-//   ),
-//   author: z.string().min(3, { message: "Author name must be at least 3 characters" }),
-//   tags: z.array(z.string()).nonempty({ message: "At least one tag is required" }),
-// });
-
-// // Create a type alias for the form values
-// type FormValues = z.infer<typeof formSchema>;
-
-// export default function Home() {
-//   const form = useForm<FormValues>({
-//     mode: "onTouched",
-//     resolver: zodResolver(formSchema),
-//     defaultValues: {
-//       title: "",
-//       content: "",
-//       author: "",
-//       tags: [],
-//     },
-//   });
-
-//   const onSubmit = async (data: FormValues) => {
-//     // Implement your form submission logic here
-//     console.log("Form submitted:", data);
-//   };
-
-//   return (
-//     <div className="max-w-3xl mx-auto py-5">
-//       <Form {...form}>
-//         <form onSubmit={form.handleSubmit(onSubmit)}>
-//           <FormField
-//             control={form.control}
-//             name="author"
-//             render={({
-//               field,
-//             }: {
-//               field: ControllerRenderProps<FormValues, "author">;
-//             }) => (
-//               <FormItem>
-//                 <FormLabel>Author</FormLabel>
-//                 <FormControl>
-//                   <input
-//                     type="text"
-//                     {...field}
-//                     className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-slate-100 text-black"
-//                   />
-//                 </FormControl>
-//                 <FormMessage />
-//               </FormItem>
-//             )}
-//           />
-
-//           <FormField
-//             control={form.control}
-//             name="tags"
-//             render={({
-//               field,
-//             }: {
-//               field: ControllerRenderProps<FormValues, "tags">;
-//             }) => (
-//               <FormItem>
-//                 <FormLabel>Tags (comma-separated)</FormLabel>
-//                 <FormControl>
-//                   <input
-//                     type="text"
-//                     value={field.value.join(", ")}
-//                     onChange={(e) =>
-//                       field.onChange(
-//                         e.target.value.split(",").map((tag) => tag.trim())
-//                       )
-//                     }
-//                     className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-slate-100 text-black"
-//                   />
-//                 </FormControl>
-//                 <FormMessage />
-//               </FormItem>
-//             )}
-//           />
-
-//           <FormField
-//             control={form.control}
-//             name="title"
-//             render={({
-//               field,
-//             }: {
-//               field: ControllerRenderProps<FormValues, "title">;
-//             }) => (
-//               <FormItem>
-//                 <FormLabel>Title</FormLabel>
-//                 <FormControl>
-//                   <input
-//                     type="text"
-//                     {...field}
-//                     className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-slate-100 text-black"
-//                   />
-//                 </FormControl>
-//                 <FormMessage />
-//               </FormItem>
-//             )}
-//           />
-
-//           <FormField
-//             control={form.control}
-//             name="content"
-//             render={({
-//               field,
-//             }: {
-//               field: ControllerRenderProps<FormValues, "content">;
-//             }) => (
-//               <FormItem>
-//                 <FormLabel>Write an Informative Blog</FormLabel>
-//                 <FormControl>
-//                   <RichTextEditor
-//                     content={field.value}
-//                     onChange={(value) => field.onChange(value)}
-//                   />
-//                 </FormControl>
-//                 <FormMessage />
-//               </FormItem>
-//             )}
-//           />
-
-//           <Button type="submit" className="mt-4 bg-slate-50 text-black">
-//             Submit
-//           </Button>
-//         </form>
-//       </Form>
-//     </div>
-//   );
-// }
